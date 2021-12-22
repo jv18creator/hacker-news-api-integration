cf0f4b0a6ea57539aa2986e409d304af43704ee3
jv18creator <jviramgama5@gmail.com>
2021-12-23 04:27:11 +0530

    fetching story data successfully

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import { Container, CssBaseline, Typography } from "@material-ui/core";
-import Navbar from "./Components/Navbar/Navbar";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Welcome from "./Pages/Welcome";
-import Footer from "./Components/Navbar/Footer.js/Footer";
+import { Container, CssBaseline, Typography } from '@material-ui/core';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Welcome from './Components/Home/Welcome';
+import Footer from './Components/Footer.js/Footer';
+import HomePage from './Pages/HomePage';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <CssBaseline />
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Welcome />
-          </Route>
-        </Switch>
-        <Footer />
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <CssBaseline />
+                <Navbar />
+                <Switch>
+                    <Route exact path="/">
+                        <HomePage />
+                    </Route>
+                </Switch>
+                <Footer />
+            </Router>
+        </>
+    );
 }
 
 export default App;
